f2006ed79ebe042d0b161cc7bb10fa37f0d23a16
Abubaker Noorzi <anoorzi123@gmail.com>
2020-02-14 00:14:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie].join
+		 str = ""
+		 str=programmer_hash[:dennis_ritchie][:known_for]
+		 str+=programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
