5b1f6f38020d1766e32cf5bfbfeebc245794d26d
Abubaker Noorzi <anoorzi123@gmail.com>
2020-02-14 00:22:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         }
      }
 
-		var = programmer_hash[:dennis_ritchie][:languages]
+		var = programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
